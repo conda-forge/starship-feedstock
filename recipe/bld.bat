2ff84fe6354d90e8d8d5234cb55df929289db9e9
Marco Rossi <developer@marco-rossi.com>
2026-06-29 23:04:16 +0200

    Try to update path

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,4 @@
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 cargo build --release --locked
 MKDIR %PREFIX%\bin
-MOVE target\release\starship.exe %PREFIX%\bin\starship.exe
+MOVE target\x86_64-pc-windows-msvc\release\starship.exe %PREFIX%\bin\starship.exe
